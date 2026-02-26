f3faf543ec2a03230c9853f3b208783ca4752389
Aditya-1406 <gargaditya674@gmail.com>
2026-02-26 10:01:09 +0530

    CRUD for the album table completed

--- Music-Store/app/views/album.py
+++ Music-Store/app/views/album.py
@@ -52,7 +52,7 @@ class AlbumCreateView(MethodView):
 class StoreView(MethodView):
     @login_required
     def get(self):
-        albums = Album.query.all()
+        albums = Album.query.order_by(Album.id.desc()).limit(4).all()
         return render_template("store.html", albums=albums)
     
 class AlbumDetailView(MethodView):
@@ -60,3 +60,49 @@ class AlbumDetailView(MethodView):
     def get(self, album_id):
         album = Album.query.get_or_404(album_id)
         return render_template("album_detail.html", album=album)
+    
+class ListAlbumAdView(MethodView):
+    @admin_required
+    def get(self):
+        page = request.args.get("page", 1, type=int)
+        albums = Album.query.order_by(Album.id.desc()).paginate(
+            page=page,
+            per_page=2,
+            error_out=False
+        )
+        return render_template("list_albumad.html", albums=albums)
+    
+class UpdateAlbum(MethodView):
+    @admin_required
+    def get(self,album_id):
+        album = Album.query.get_or_404(album_id)
+        return render_template("update_album.html",album=album)
+    
+    @admin_required
+    def post(self,album_id):
+        album = Album.query.get_or_404(album_id)
+        album.title = request.form.get("title")
+        album.artist = request.form.get("artist")
+        album.copies = request.form.get("copies")
+        album.amount = request.form.get("amount")
+
+        
+        cover = request.files.get("cover")
+        if cover and cover.filename != "":
+            filename = save_cover_image(cover)
+            if filename:
+                album.cover_image = filename
+
+        db.session.commit()
+        flash("Album updated successfully!")
+        return redirect(url_for("list_albums"))
+    
+class DeleteAlbumView(MethodView):
+    @admin_required
+    def post(self,album_id):
+        album = Album.query.get_or_404(album_id)
+        db.session.delete(album)
+        db.session.commit()
+        flash("Album updated successfully!")
+        return redirect(url_for("list_albums"))
+
--- Music-Store/run.py
+++ Music-Store/run.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 from flask_wtf.csrf import CSRFProtect
 from app.views.auth import SignupView,OTPView,LoginView,LogoutView
-from app.views.album import AlbumCreateView,AlbumDetailView,StoreView
+from app.views.album import AlbumCreateView,AlbumDetailView,StoreView,ListAlbumAdView,UpdateAlbum,DeleteAlbumView
 from app.extensions import db,mail
 
 
 app = Flask(__name__,template_folder="app/templates")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:PASS@localhost:3306/music_store'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:PD40123@localhost:3306/music_store'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "supersecretkeydontknowwh@tiswrllten"
 
@@ -23,7 +23,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'gargaditya674@gmail.com'
 app.config['MAIL_DEFAULT_SENDER'] = 'gargaditya674@gmail.com'
-app.config['MAIL_PASSWORD'] = 'PASS'
+app.config['MAIL_PASSWORD'] = 'PS'
 mail.init_app(app)
 
 
@@ -39,6 +39,9 @@ app.add_url_rule("/logout",view_func=LogoutView.as_view("logout"),methods=["GET"
 app.add_url_rule("/create-album", view_func=AlbumCreateView.as_view("create_album"))
 app.add_url_rule("/", view_func=StoreView.as_view("store"))
 app.add_url_rule("/album/<int:album_id>", view_func=AlbumDetailView.as_view("album_detail"))
+app.add_url_rule("/list-albums", view_func=ListAlbumAdView.as_view("list_albums"))
+app.add_url_rule("/update-album/<int:album_id>", view_func=UpdateAlbum.as_view("update_album"),methods=["GET","POST"])
+app.add_url_rule("/delete-album/<int:album_id>", view_func=DeleteAlbumView.as_view("delete_album"),methods=["GET","POST"])
 
 
 
