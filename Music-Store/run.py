ce36836265032adab2894b5728b7e81f19ec9c30
Aditya-1406 <gargaditya674@gmail.com>
2026-02-26 10:43:48 +0530

    some tweaks in ui

--- Music-Store/run.py
+++ Music-Store/run.py
@@ -6,7 +6,7 @@ from app.extensions import db,mail
 
 
 app = Flask(__name__,template_folder="app/templates")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:PD40123@localhost:3306/music_store'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:PS%40123@localhost:3306/music_store'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "supersecretkeydontknowwh@tiswrllten"
 
@@ -23,7 +23,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'gargaditya674@gmail.com'
 app.config['MAIL_DEFAULT_SENDER'] = 'gargaditya674@gmail.com'
-app.config['MAIL_PASSWORD'] = 'PS'
+app.config['MAIL_PASSWORD'] = 'PASS'
 mail.init_app(app)
 
 
